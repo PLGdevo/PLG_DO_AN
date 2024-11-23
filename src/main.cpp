273067fe9ac201eb6093f9683311b7935039bc68
PLGdevo <minhphong270704@gmail.com>
2024-11-23 16:56:23 +0700

    ok rùi nè

--- src/main.cpp
+++ src/main.cpp
@@ -34,80 +34,6 @@ void sosanh(void);
 void chedo(void);
 void bandau(void);
 
-// //_____________wifi congig_______________//
-// #define BUTTON_PIN 0
-// #if defined(BUTTON_PIN)
-// // Active low (false), Active high (true)
-// #define BUTTON_INVERT false
-// #define BUTTON_HOLD_TIMEOUT 3000UL
-
-// // This directive is used to specify whether the configuration should be erased.
-// // If it's set to true, the configuration will be erased.
-// #define ERA_ERASE_CONFIG false
-// #endif
-// #if defined(BUTTON_PIN)
-// #include <pthread.h>
-// #include <ERa/ERaButton.hpp>
-// #endif
-// const char ssid[] = "ZenThenk";
-// const char pass[] = "vanthanh04";
-
-// WiFiClient mbTcpClient;
-// #if defined(BUTTON_PIN)
-// ERaButton button;
-// pthread_t pthreadButton;
-// static void *handlerButton(void *args)
-// {
-//   for (;;)
-//   {
-//     button.run();
-//     ERaDelay(10);
-//   }
-//   pthread_exit(NULL);
-// }
-// #if ERA_VERSION_NUMBER >= ERA_VERSION_VAL(1, 2, 0)
-// static void eventButton(uint8_t pin, ButtonEventT event)
-// {
-//   if (event != ButtonEventT::BUTTON_ON_HOLD)
-//   {
-//     return;
-//   }
-//   ERa.switchToConfig(ERA_ERASE_CONFIG);
-//   (void)pin;
-// }
-// #else
-// static void eventButton(ButtonEventT event)
-// {
-//   if (event != ButtonEventT::BUTTON_ON_HOLD)
-//   {
-//     return;
-//   }
-//   ERa.switchToConfig(ERA_ERASE_CONFIG);
-// }
-// #endif
-
-// void initButton()
-// {
-//   pinMode(BUTTON_PIN, INPUT);
-//   button.setButton(BUTTON_PIN, digitalRead, eventButton,
-//                    BUTTON_INVERT)
-//       .onHold(BUTTON_HOLD_TIMEOUT);
-//   pthread_create(&pthreadButton, NULL, handlerButton, NULL);
-// }
-// #endif
-
-// ERA_CONNECTED()
-// {
-//   ERA_LOG("ERa", "ERa connected!");
-//   Serial.println("ERa connected!");
-// }
-// ERA_DISCONNECTED()
-// {
-//   bandau();
-//   ERA_LOG("ERa", "ERa disconnected!");
-//   Serial.println("ERa disconnected!");
-// }
-// //_______________wifi-confic-end________________________//
 //_____________wifi congig_______________//
 #define BUTTON_PIN 0
 #if defined(BUTTON_PIN)
@@ -285,8 +211,6 @@ void setup()
   // ERa.setScanWiFi(false);
   ERa.begin(ssid, pass);
   timer.setInterval(1000L, timerEvent);
-
- 
 }
 
 void loop()
